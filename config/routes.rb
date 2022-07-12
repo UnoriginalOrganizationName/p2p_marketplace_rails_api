b13b45db00a4d88beb06518403035a24cf91eaf5
Timothy Marias <timothyjamesmarias@gmail.com>
2022-07-12 15:13:55 -0700

    added admin endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,18 @@ Rails.application.routes.draw do
   resources :requests
   resources :listings
   resources :users
+  resources :admin
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Health check endpoint
+  get "/health", to: proc { [200, {}, ["OK"]] }
+
+  #authentication
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+
+  #admin dashboardd
+  get "/admin", to: "admin#index"
 
   # Defines the root path route ("/")
   root "home#index"
